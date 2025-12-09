f62ee6022e3a38db5f193121661d92b95d5a7edf
yelobat <lukeanthonyholland@gmail.com>
2025-12-09 02:27:02 +0000

    added slip.rs and updated lib.rs.
    slip.rs is now a dedicated file for slip decoding, it will be a
    bevy plugin that exposes some systems as part of the Update Systemset
    (maybe?). The lib.rs file now includes a Update SystemSet which
    allows us to partition systems into specific orders based on their
    features.

--- crates/bevy_dirtywave_m8/src/lib.rs
+++ crates/bevy_dirtywave_m8/src/lib.rs
@@ -2,8 +2,18 @@
 
 mod command;
 mod serialport;
+mod slip;
 
 use bevy::prelude::*;
+
+/// The states of operation for which the Plugin will operate.
+#[derive(SystemSet, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Hash)]
+enum DirtywaveM8UpdateSystems {
+    Input,
+    SerialRead,
+    DisplayRender,
+    Update,
+}
 
 /// If no port is defined, this is the assigned default one.
 const DEFAULT_DIRTYWAVE_M8_PORT: &'static str = "/dev/ttyACM0";
@@ -12,6 +22,18 @@ const DEFAULT_DIRTYWAVE_M8_PORT: &'static str = "/dev/ttyACM0";
 pub struct DirtywaveM8Plugin(pub String);
 impl Plugin for DirtywaveM8Plugin {
     fn build(&self, app: &mut App) {
+        // Configure the M8 Update SystemSet
+        app.configure_sets(
+            Update,
+            (
+                DirtywaveM8UpdateSystems::Input,
+                DirtywaveM8UpdateSystems::SerialRead,
+                DirtywaveM8UpdateSystems::DisplayRender,
+                DirtywaveM8UpdateSystems::Update,
+            ),
+        );
+
+        // Add the Serial Interaction Plugin.
         app.add_plugins(serialport::M8SerialPlugin {
             preferred_device: self.0.clone().into(),
         });
---
+++ crates/bevy_dirtywave_m8/src/slip.rs
@@ -0,0 +1,152 @@
+//! Slip Implementation based on: <https://github.com/laamaa/m8c/src/backends/slip.c>
+
+// SLIP Protocol Constants
+const SLIP_END: u8 = 0xC0;
+const SLIP_ESC: u8 = 0xDB;
+const SLIP_ESC_END: u8 = 0xDC;
+const SLIP_ESC_ESC: u8 = 0xDD;
+
+/// Errors that can occur during SLIP decoding.
+#[derive(Debug, Clone, PartialEq)]
+pub enum SlipError {
+    BufferOverflow,
+    InvalidPacket,
+    UnknownEscapedByte(u8),
+}
+
+/// SLIP Decoder State.
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum State {
+    Normal,
+    Escaped,
+}
+
+/// SLIP Decoder.
+#[derive(Debug)]
+pub struct SlipDecoder {
+    buffer: Vec<u8>,
+    capacity: usize,
+    state: State,
+}
+
+impl SlipDecoder {
+    /// Creates a new SlipDecoder with maximum [capacity].
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            buffer: Vec::with_capacity(capacity),
+            capacity,
+            state: State::Normal,
+        }
+    }
+
+    /// Resets the SLIP Decoder's internal buffer and state.
+    pub fn reset(&mut self) {
+        self.buffer.clear();
+        self.state = State::Normal;
+    }
+
+    /// Append a byte to the internal buffer, respecting the maximum capacity.
+    fn put_byte(&mut self, byte: u8) -> Result<(), SlipError> {
+        if self.buffer.len() >= self.capacity {
+            self.reset();
+            Err(SlipError::BufferOverflow)
+        } else {
+            self.buffer.push(byte);
+            self.state = State::Normal;
+            Ok(())
+        }
+    }
+
+    pub fn decode(&mut self, stream: &[u8]) -> Result<(), SlipError> {
+        self.reset(); // <- Not sure if this line should be added.
+        Ok(stream.iter().try_for_each(|chr| self.decode_byte(*chr))?)
+    }
+
+    fn decode_byte(&mut self, byte: u8) -> Result<(), SlipError> {
+        match self.state {
+            State::Normal => match byte {
+                SLIP_END => {
+                    // This is where the current message should actually
+                    // be processed.
+                    self.reset();
+                    Ok(())
+                }
+                SLIP_ESC => {
+                    self.state = State::Escaped;
+                    Ok(())
+                }
+                _ => {
+                    self.put_byte(byte)?;
+                    Ok(())
+                }
+            },
+            State::Escaped => match byte {
+                SLIP_ESC_END => {
+                    self.put_byte(SLIP_END)?;
+                    Ok(())
+                }
+                SLIP_ESC_ESC => {
+                    self.put_byte(SLIP_ESC)?;
+                    Ok(())
+                }
+                _ => {
+                    self.reset();
+                    return Err(SlipError::UnknownEscapedByte(byte));
+                }
+            },
+        }
+    }
+}
+
+#[test]
+fn decoder_init_test() {
+    let decoder = SlipDecoder::new(1024);
+    assert_eq!(decoder.buffer, []);
+    assert_eq!(decoder.state, State::Normal);
+    assert_eq!(decoder.capacity, 1024);
+}
+
+#[test]
+fn decoder_insertion_test() {
+    let mut decoder = SlipDecoder::new(1024);
+    decoder.put_byte(b'c').unwrap();
+    assert_eq!(decoder.buffer, [b'c']);
+    assert_eq!(decoder.buffer.len(), 1);
+    decoder.put_byte(b'd').unwrap();
+    assert_eq!(decoder.buffer, [b'c', b'd']);
+    assert_eq!(decoder.buffer.len(), 2);
+    decoder.put_byte(b'e').unwrap();
+    assert_eq!(decoder.buffer, [b'c', b'd', b'e']);
+    assert_eq!(decoder.buffer.len(), 3);
+}
+
+#[test]
+fn decoder_decode_test() {
+    let mut decoder = SlipDecoder::new(1024);
+    decoder.decode(&[SLIP_ESC, SLIP_ESC_END]).unwrap();
+    assert_eq!(decoder.buffer, [SLIP_END]);
+    decoder.decode(&[SLIP_ESC, SLIP_ESC_END, SLIP_END]).unwrap();
+    assert_eq!(decoder.buffer, []);
+    decoder
+        .decode(&[
+            SLIP_ESC,
+            SLIP_ESC_END,
+            b'\xfe',
+            b'\xff',
+            b'\x77',
+            SLIP_ESC,
+            SLIP_ESC_ESC,
+        ])
+        .unwrap();
+    assert_eq!(
+        decoder.buffer,
+        [SLIP_END, b'\xfe', b'\xff', b'\x77', SLIP_ESC]
+    );
+}
+
+#[test]
+#[should_panic]
+fn decoder_invalid_esc_test() {
+    let mut decoder = SlipDecoder::new(1024);
+    decoder.decode(&[SLIP_ESC, b'\x00']).unwrap();
+}
